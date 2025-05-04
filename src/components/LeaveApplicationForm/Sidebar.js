4acb1759da3b6e9f69528d0f3cdb79c47d638d87
Ritam Bhattacharya <imrit33@gmail.com>
2025-05-04 10:52:30 +0530

    sidebar fixed

--- src/components/LeaveApplicationForm/Sidebar.js
+++ src/components/LeaveApplicationForm/Sidebar.js
@@ -1,57 +1,39 @@
-// import React from "react";
-// import "../../styles/Sidebar.css";
-
-// function Sidebar() {
-//   return (
-//     <div className="sidebar">
-//       <h2>Employee Leave Management</h2>
-//       <ul>
-//         <li className="active">Apply For Leave</li>
-//         <li>View Remaining Leaves</li>
-//         <li>Show Leave History</li>
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default Sidebar;
-
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { FaUserCircle, FaCalendarAlt } from "react-icons/fa";
 import "../../styles/Sidebar.css"; // Optional: put your sidebar styles here
 
-
-
 function Sidebar() {
-  const location=useLocation();
+  const location = useLocation();
+  const navigate = useNavigate();
 
   return (
     <div className="sidebar">
       <h2>Employee Leave Management</h2>
       <ul>
-
-        <li className={location.pathname === "/apply" ? "active" : ""}>
-          <Link to="/apply" className="link">
-            <FaUserCircle /> Apply For Leave
-          </Link>
+        <li
+          className={location.pathname === "/apply" ? "active" : ""}
+          onClick={() => navigate("/apply")}
+        >
+          <FaUserCircle /> Apply For Leave
         </li>
-        <li className={location.pathname === "/remaining" ? "active" : ""}>
-          <Link to="/remaining" className="link">
-            <FaCalendarAlt /> View Remaining Leaves
-          </Link>
+        <li
+          className={location.pathname === "/remaining" ? "active" : ""}
+          onClick={() => navigate("/remaining")}
+        >
+          <FaCalendarAlt /> View Remaining Leaves
         </li>
-        <li className={location.pathname === "/history" ? "active" : ""}>
-          <Link to="/history" className="link">
-            <FaCalendarAlt /> Show Leave History
-          </Link>
+        <li
+          className={location.pathname === "/history" ? "active" : ""}
+          onClick={() => navigate("/history")}
+        >
+          <FaCalendarAlt /> Show Leave History
         </li>
       </ul>
     </div>
   );
-};
-
-
+}
 
 export default Sidebar;
